54dd8a47659a13ae5c93696ff6e5546212620791
omgtropuhq-dot <omgtropuhq@gmail.com>
2026-03-25 18:21:55 +0100

    Update autoshop_bot.py

--- autoshop_bot.py
+++ autoshop_bot.py
@@ -32,10 +32,10 @@ from flask import Flask, request as flask_request, jsonify
 # ==========================================
 # ⚙️ CONFIGURATION — REMPLIS ICI
 # ==========================================
-BOT_TOKEN      = "TON_TOKEN_TELEGRAM_BOT"
-PLISIO_API_KEY = "TA_CLE_API_PLISIO"     # plisio.net → API Keys
-ADMIN_IDS      = [123456789]
-LOG_GROUP_ID   = -100987654321
+BOT_TOKEN      = "8628435813:AAG-RpDUGSpNaTWbPFgwaQuJe7ffRHY7E24"
+PLISIO_API_KEY = "b-WCYaL8vgyJobhvc-0eEt3nnwkHmPkmJhUdXB8JJeYR7DNegbyJpo0Z9ngKATNM"     # plisio.net → API Keys
+ADMIN_IDS      = [8704755112]
+LOG_GROUP_ID   = -5142753842
 DB_PATH        = "shop.db"
 PLISIO_API     = "https://plisio.net/api/v1"
 WEBHOOK_PORT   = 5000
