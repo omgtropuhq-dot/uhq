4ba35a3505cc7a5e62854c5186d892c94423d685
omgtropuhq-dot <omgtropuhq@gmail.com>
2026-03-26 01:11:36 +0100

    Update plesk_shop.py

--- plesk_shop.py
+++ plesk_shop.py
@@ -31,10 +31,10 @@ import threading
 # ==========================================
 # ⚙️  CONFIGURATION
 # ==========================================
-BOT_TOKEN      = "TON_TOKEN_TELEGRAM_BOT"
-PLISIO_API_KEY = "TA_CLE_API_PLISIO"
-ADMIN_IDS      = [123456789]          # Ton ID Telegram
-LOG_GROUP_ID   = -100987654321        # ID groupe logs (ou ton propre ID)
+BOT_TOKEN      = "8628435813:AAG-RpDUGSpNaTWbPFgwaQuJe7ffRHY7E24"
+PLISIO_API_KEY = "b-WCYaL8vgyJobhvc-0eEt3nnwkHmPkmJhUdXB8JJeYR7DNegbyJpo0Z9ngKATNM"
+ADMIN_IDS      = [8704755112]          # Ton ID Telegram
+LOG_GROUP_ID   = -5142753842        # ID groupe logs (ou ton propre ID)
 DB_PATH        = "shop.db"
 PLISIO_API     = "https://plisio.net/api/v1"
 PLESK_URL      = "https://hungry-boyd.82-165-77-124.plesk.page/"
